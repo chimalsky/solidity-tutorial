e461af38c5c5256e467eb2bdd5abc1bdb060b9c4
chimalsky <ich.bin.abe@gmail.com>
2023-08-03 21:41:31 -0400

    foo method of vanilla ethers transaction

--- src/createWallet.js
+++ src/createWallet.js
@@ -2,6 +2,7 @@ const ethers = require("ethers");
 
 const wallet = ethers.Wallet.createRandom();
 
+console.log(`Private: ${wallet.privateKey}`);
 console.log(`Mnemonic: ${wallet.mnemonic.phrase}`);
 console.log(`Address: ${wallet.address}`);
 
